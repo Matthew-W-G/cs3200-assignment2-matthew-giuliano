7a9d1bf4c090e1c0bc8b8aca88f7f2a79f29be99
Matthew-W-G <68626329+Matthew-W-G@users.noreply.github.com>
2020-10-20 13:09:01 -0400

    Update pagesInserts.sql

--- Inserts/pagesInserts.sql
+++ Inserts/pagesInserts.sql
@@ -4,5 +4,5 @@ LIMIT 0, 1000
 
 -- Date: 2020-10-20 12:46
 */
-INSERT INTO `` (`id`,`title`,`description`,`created`,`updated`,`views`,`website_id`) VALUES (234,'CNETAbout','Website description','0000-00-00 00:00:00','0000-00-00 00:00:00',234545,678);
-INSERT INTO `` (`id`,`title`,`description`,`created`,`updated`,`views`,`website_id`) VALUES (456,'CNETPreferences','Where users can configure their preferences','0000-00-00 00:00:00','0000-00-00 00:00:00',456776,456);
+INSERT INTO `` (`id`,`title`,`description`,`created`,`updated`,`views`,`website_id`) VALUES (234,'CNETCNETAbout','Website description','0000-00-00 00:00:00','0000-00-00 00:00:00',234545,678);
+INSERT INTO `` (`id`,`title`,`description`,`created`,`updated`,`views`,`website_id`) VALUES (456,'CNETCNETPreferences','Where users can configure their preferences','0000-00-00 00:00:00','0000-00-00 00:00:00',456776,456);
