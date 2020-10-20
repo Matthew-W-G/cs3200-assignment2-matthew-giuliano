f00429cf87d5322e00eb9bc1069c65e6b9522f7a
Matthew-W-G <68626329+Matthew-W-G@users.noreply.github.com>
2020-10-20 13:08:01 -0400

    Add files via upload

---
+++ Inserts/developerRolesPrivilegesInserts.sql
@@ -0,0 +1,56 @@
+/*
+-- Query: SELECT * FROM heroku_f3d0da64923a25a.developer_roles_and_privileges
+LIMIT 0, 1000
+
+-- Date: 2020-10-20 13:07
+*/
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Alice','Wonder','alice','\n\nalice@wonder.com\n\n\nalice@wonder.com\nalice@w','Facebook',1234234,'CNETCNETAbout',234545,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Alice','Wonder','alice','\n\nalice@wonder.com\n\n\nalice@wonder.com\nalice@w','Facebook',1234234,'CNETCNETPreferences',456776,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Bob','Marley','bob','bob','Facebook',1234234,'CNETCNETAbout',234545,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Bob','Marley','bob','bob','Facebook',1234234,'CNETCNETPreferences',456776,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Charles','Garcia','charlie','charlie','Facebook',1234234,'CNETCNETAbout',234545,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Charles','Garcia','charlie','charlie','Facebook',1234234,'CNETCNETPreferences',456776,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Dan','Martin','dan','dan','Facebook',1234234,'CNETCNETAbout',234545,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Dan','Martin','dan','dan','Facebook',1234234,'CNETCNETPreferences',456776,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Ed','Karaz','ed','ed','Facebook',1234234,'CNETCNETAbout',234545,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Ed','Karaz','ed','ed','Facebook',1234234,'CNETCNETPreferences',456776,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Alice','Wonder','alice','\n\nalice@wonder.com\n\n\nalice@wonder.com\nalice@w','Twitter',4321543,'CNETCNETAbout',234545,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Alice','Wonder','alice','\n\nalice@wonder.com\n\n\nalice@wonder.com\nalice@w','Twitter',4321543,'CNETCNETPreferences',456776,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Bob','Marley','bob','bob','Twitter',4321543,'CNETCNETAbout',234545,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Bob','Marley','bob','bob','Twitter',4321543,'CNETCNETPreferences',456776,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Charles','Garcia','charlie','charlie','Twitter',4321543,'CNETCNETAbout',234545,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Charles','Garcia','charlie','charlie','Twitter',4321543,'CNETCNETPreferences',456776,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Dan','Martin','dan','dan','Twitter',4321543,'CNETCNETAbout',234545,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Dan','Martin','dan','dan','Twitter',4321543,'CNETCNETPreferences',456776,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Ed','Karaz','ed','ed','Twitter',4321543,'CNETCNETAbout',234545,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Ed','Karaz','ed','ed','Twitter',4321543,'CNETCNETPreferences',456776,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Alice','Wonder','alice','\n\nalice@wonder.com\n\n\nalice@wonder.com\nalice@w','Wikipedia',3456654,'CNETCNETAbout',234545,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Alice','Wonder','alice','\n\nalice@wonder.com\n\n\nalice@wonder.com\nalice@w','Wikipedia',3456654,'CNETCNETPreferences',456776,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Bob','Marley','bob','bob','Wikipedia',3456654,'CNETCNETAbout',234545,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Bob','Marley','bob','bob','Wikipedia',3456654,'CNETCNETPreferences',456776,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Charles','Garcia','charlie','charlie','Wikipedia',3456654,'CNETCNETAbout',234545,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Charles','Garcia','charlie','charlie','Wikipedia',3456654,'CNETCNETPreferences',456776,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Dan','Martin','dan','dan','Wikipedia',3456654,'CNETCNETAbout',234545,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Dan','Martin','dan','dan','Wikipedia',3456654,'CNETCNETPreferences',456776,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Ed','Karaz','ed','ed','Wikipedia',3456654,'CNETCNETAbout',234545,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Ed','Karaz','ed','ed','Wikipedia',3456654,'CNETCNETPreferences',456776,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Alice','Wonder','alice','\n\nalice@wonder.com\n\n\nalice@wonder.com\nalice@w','CNN',6543345,'CNETCNETAbout',234545,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Alice','Wonder','alice','\n\nalice@wonder.com\n\n\nalice@wonder.com\nalice@w','CNN',6543345,'CNETCNETPreferences',456776,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Bob','Marley','bob','bob','CNN',6543345,'CNETCNETAbout',234545,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Bob','Marley','bob','bob','CNN',6543345,'CNETCNETPreferences',456776,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Charles','Garcia','charlie','charlie','CNN',6543345,'CNETCNETAbout',234545,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Charles','Garcia','charlie','charlie','CNN',6543345,'CNETCNETPreferences',456776,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Dan','Martin','dan','dan','CNN',6543345,'CNETCNETAbout',234545,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Dan','Martin','dan','dan','CNN',6543345,'CNETCNETPreferences',456776,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Ed','Karaz','ed','ed','CNN',6543345,'CNETCNETAbout',234545,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Ed','Karaz','ed','ed','CNN',6543345,'CNETCNETPreferences',456776,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Alice','Wonder','alice','\n\nalice@wonder.com\n\n\nalice@wonder.com\nalice@w','Gizmodo',4322345,'CNETCNETAbout',234545,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Alice','Wonder','alice','\n\nalice@wonder.com\n\n\nalice@wonder.com\nalice@w','Gizmodo',4322345,'CNETCNETPreferences',456776,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Bob','Marley','bob','bob','Gizmodo',4322345,'CNETCNETAbout',234545,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Bob','Marley','bob','bob','Gizmodo',4322345,'CNETCNETPreferences',456776,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Charles','Garcia','charlie','charlie','Gizmodo',4322345,'CNETCNETAbout',234545,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Charles','Garcia','charlie','charlie','Gizmodo',4322345,'CNETCNETPreferences',456776,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Dan','Martin','dan','dan','Gizmodo',4322345,'CNETCNETAbout',234545,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Dan','Martin','dan','dan','Gizmodo',4322345,'CNETCNETPreferences',456776,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Ed','Karaz','ed','ed','Gizmodo',4322345,'CNETCNETAbout',234545,'0000-00-00 00:00:00');
+INSERT INTO `` (`firstName`,`lastName`,`username`,`password`,`name`,`visits`,`title`,`views`,`created`) VALUES ('Ed','Karaz','ed','ed','Gizmodo',4322345,'CNETCNETPreferences',456776,'0000-00-00 00:00:00');
